3b0c498e2167639211051ef34a775c1731566053
vdlight <jensavd@gmail.com>
2017-09-19 19:02:29 +0200

    Skapande av projekt

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace StatGet_CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Col_Time = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Col_Res = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Col_Stat = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Col_Lag = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Col_Målgörare = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Col_Ass1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Col_Ass2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Col_Time,
+            this.Col_Res,
+            this.Col_Stat,
+            this.Col_Lag,
+            this.Col_Målgörare,
+            this.Col_Ass1,
+            this.Col_Ass2});
+            this.listView1.Location = new System.Drawing.Point(12, 140);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(884, 201);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Col_Time
+            // 
+            this.Col_Time.Text = "Time";
+            this.Col_Time.Width = 70;
+            // 
+            // Col_Res
+            // 
+            this.Col_Res.Text = "Res";
+            this.Col_Res.Width = 70;
+            // 
+            // Col_Stat
+            // 
+            this.Col_Stat.Text = "Stat";
+            this.Col_Stat.Width = 70;
+            // 
+            // Col_Lag
+            // 
+            this.Col_Lag.Text = "Team";
+            this.Col_Lag.Width = 70;
+            // 
+            // Col_Målgörare
+            // 
+            this.Col_Målgörare.Text = "Scorer";
+            this.Col_Målgörare.Width = 200;
+            // 
+            // Col_Ass1
+            // 
+            this.Col_Ass1.Text = "Assist 1";
+            this.Col_Ass1.Width = 200;
+            // 
+            // Col_Ass2
+            // 
+            this.Col_Ass2.Text = "Assist 2";
+            this.Col_Ass2.Width = 200;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1000, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 386);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(884, 140);
+            this.treeView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1174, 601);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Col_Time;
+        private System.Windows.Forms.ColumnHeader Col_Res;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColumnHeader Col_Stat;
+        private System.Windows.Forms.ColumnHeader Col_Lag;
+        private System.Windows.Forms.ColumnHeader Col_Målgörare;
+        private System.Windows.Forms.ColumnHeader Col_Ass1;
+        private System.Windows.Forms.ColumnHeader Col_Ass2;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace StatGet_CSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<PeriodGoal> periodGoals;
+        string HTMLdata;
+
+
+        public class PeriodGoal
+        {
+            public string _time;
+            public string _res;
+            public string _Stat;
+            public string _team;
+            public string _scorer;
+            public string _ass1;
+            public string _ass2;
+
+            public PeriodGoal(string time, string res, string stat, string team, string scorer, string ass1, string ass2)
+            {
+                _time = time;
+                _res = res;
+                _Stat = stat;
+                _team = team;
+                _scorer = scorer;
+                _ass1 = ass1;
+                _ass2 = ass2;
+            }
+
+            public PeriodGoal(string time, string res, string stat, string team, string scorer, string ass1)
+            {
+                _time = time;
+                _res = res;
+                _Stat = stat;
+                _team = team;
+                _scorer = scorer;
+                _ass1 = ass1;
+                _ass2 = "N/A";
+            }
+
+            public PeriodGoal(string time, string res, string stat, string team, string scorer)
+            {
+                _time = time;
+                _res = res;
+                _Stat = stat;
+                _team = team;
+                _scorer = scorer;
+                _ass1 = "N/A";
+                _ass2 = "N/A";
+            }
+        }
+
+        public void updateGoalsList()
+        {
+            listView1.Columns.Add("Time", 70);
+            listView1.Columns.Add("Res", 70);
+            listView1.Columns.Add("Stat", 70);
+            listView1.Columns.Add("Team", 70);
+            listView1.Columns.Add("Scorere", 200);
+            listView1.Columns.Add("Assist 1", 200);
+            listView1.Columns.Add("Assist 2", 200);
+
+            ListViewItem row;
+            foreach (var goal in periodGoals) {
+                row = new ListViewItem(goal._time);
+                row.SubItems.Add(goal._res);
+                row.SubItems.Add(goal._Stat);
+                row.SubItems.Add(goal._team);
+                row.SubItems.Add(goal._scorer);
+                row.SubItems.Add(goal._ass1);
+                row.SubItems.Add(goal._ass2);
+
+                listView1.Items.Add(row);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            periodGoals = new List<PeriodGoal>();
+
+            periodGoals.Add(new PeriodGoal("1:00", "1-0", "EQ", "H", "jensa", "mupp1", "mupp2"));
+            periodGoals.Add(new PeriodGoal("2:00", "2-0", "EQ", "H", "jensa", "mupp1"));
+            periodGoals.Add(new PeriodGoal("2:00", "2-0", "EQ", "H", "jensa"));
+
+           listView1.Clear();
+           updateGoalsList();
+
+
+            readHTML();
+            generateTree();
+
+        }
+
+        public void readHTML()
+        {
+            string pageAddr = "http://stats.swehockey.se/Game/Events/206736";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(pageAddr);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                if (response.CharacterSet == null)
+                {
+                    readStream = new StreamReader(receiveStream);
+                }
+                else
+                {
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                HTMLdata = readStream.ReadToEnd();
+
+                response.Close();
+                readStream.Close();
+            }
+        }
+
+        public void generateTree() {
+            int nodeCnt = 0;
+            int treeDepthParents[50] = { 0 };
+            int currentParent = 0;
+            treeForm->TreeView1->Items->Clear();
+
+            // find next tag
+            int pos;
+            int start = 0;
+            bool endSign;
+            int startPos;
+            int endPos;
+            AnsiString tag = "";
+            AnsiString buff;
+            bool tagNameEnded;
+            TTreeNode* currentNode;
+
+            // First child and its children
+            treeForm->TreeView1->Items->Add(NULL, "HTML Base");
+            buff = rawHTML;
+
+            currentParent = 0;
+            treeDepthParents[currentParent] = 0;
+            bool modified;
+            bool dataErased;
+            bool endRowFound = true;
+
+            // find next tag
+            while (0 != (startPos = buff.Pos("<")))
+            {
+                endPos = startPos;
+                startPos++;
+                tag = "";
+                tagNameEnded = false;
+
+
+                // if data exists on tag
+                AnsiString data = "";
+                if (startPos > 2 && nodeCnt > 0)
+                {
+                    AnsiString data = buff.SubString(1, startPos - 2);
+                    dataErased = false;
+
+                    do
+                    {
+                        modified = false;
+                        if (isWhiteSpace(data[1]) || isBadChar(data[1]))
+                        {
+                            if (data.Length() == 1)
+                            {
+                                dataErased = true;
+                            }
+                            data.Delete(1, 1);
+                            modified = true;
+                        }
+                    } while (!dataErased && modified);
+
+                    if (!dataErased)
+                    {
+                        do
+                        {
+                            modified = false;
+                            int b = data.Length();
+                            if (isWhiteSpace(data[data.Length()]) || isBadChar(data[data.Length()]))
+                            {
+                                if (data.Length() == 1)
+                                {
+                                    dataErased = true;
+                                }
+                                data.Delete(data.Length(), 1);
+                                modified = true;
+                            }
+                        } while (!dataErased && modified);
+                    }
+
+                    if (!dataErased)
+                    {
+                        do
+                        {
+                            modified = false;
+                            for (int i = 1; i <= data.Length(); i++)
+                            {
+                                if (isBadChar(data[i]))
+                                {
+                                    if (data.Length() == 1)
+                                    {
+                                        dataErased = true;
+                                    }
+                                    data.Delete(i, 1);
+                                    i--;
+                                    modified = true;
+                                }
+                                else if (isWhiteSpace(data[i]) && i > 1)
+                                {
+                                    if (isWhiteSpace(data[i - 1]))
+                                    {
+                                        if (data.Length() == 1)
+                                        {
+                                            dataErased = true;
+                                        }
+
+                                        data.Delete(i, 1);
+                                        i--;
+                                        modified = true;
+                                    }
+                                }
+                            }
+                        } while (modified && !dataErased);
+
+                        if (!dataErased)
+                        {
+                            currentNode->Text = currentNode->Text + ": " + data;
+                        }
+                    }
+                }
+
+                while (buff[++endPos] != '>')
+                {
+                    if (buff[endPos] != '/')
+                    {
+                        if (buff[endPos] == ' ')
+                        {
+                            tagNameEnded = true;
+                        }
+
+                        if (!tagNameEnded)
+                        {
+                            tag += buff[endPos];
+                        }
+
+                    }
+                }
+                endSign = buff[startPos] == '/';
+
+                if (!endSign)
+                {
+                    if (tag.AnsiCompare("tr") == 0)
+                    {
+                        if (!endRowFound)
+                        {
+                            // make end row, code missed that apparently
+                            currentParent--;
+                        }
+                        endRowFound = false;
+                    }
+
+                    currentNode = treeForm->TreeView1->Items->AddChild(treeForm->TreeView1->Items->Item[treeDepthParents[currentParent]], tag.c_str());
+                    nodeCnt++;
+                    if (tagIsNewParent(tag))
+                    {
+                        currentParent++;
+                        treeDepthParents[currentParent] = nodeCnt;
+                    }
+
+                }
+                else {
+                    if (tag.AnsiCompare("tr") == 0)
+                    {
+                        endRowFound = true;
+                    }
+
+                    if (tagIsNewParent(tag))
+                    {
+                        currentParent--;
+                    }
+                }
+
+                buff.Delete(1, endPos);
+            }
+
+
+        }
+        
+    }
+}
